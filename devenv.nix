f099137a6f931c0c719f713b4116b32b48f280f4
Bor Hodošček <dev@bor.space>
2024-12-02 18:11:37 +0900

    fix: added typst fonts envvar

--- devenv.nix
+++ devenv.nix
@@ -13,6 +13,7 @@
   # https://devenv.sh/scripts/
   scripts.compile-thesis.exec = "${pkgs.bash}/bin/bash make.sh thesis";
 
+  env.TYPST_FONT_PATHS = "${pkgs.source-han-serif}/share/fonts/opentype/source-han-serif:${pkgs.source-han-sans}/share/fonts/opentype/source-han-sans";
   enterShell = ''
     git --version
   '';
